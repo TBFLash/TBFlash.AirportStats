bb1b39382254c6a247200d76f979d65e65cb9232
Jason White <jason.white10@gmail.com>
2020-05-12 16:11:54 -0400

    Add project files.

---
+++ Mod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimAirport.Modding.Base;
+using SimAirport.Modding.Settings;
+
+namespace $safeprojectname$
+{
+    public class Mod : BaseMod
+    {
+        public override string Name => "";
+
+        public override string InternalName => "";
+
+        public override string Description => "";
+
+        public override string Author => "";
+
+        public override SettingManager SettingManager { get; set; }
+
+        public override void OnTick()
+        {
+            
+        }
+    }
+}
